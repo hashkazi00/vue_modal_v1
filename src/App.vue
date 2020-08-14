00787f87dda7b7845ca82504857f616607e10c65
hashkazi00 <sahilkazi01@hotmail.com>
2020-08-14 20:04:52 +0530

    second commit

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <Modal v-if="showModal" @subOne="subOne" @addOne="addOne" :num="num" />
+    <div class="container">
+      <button v-if="!showModal" @click="showModal = !showModal" class="viewModal">Click Me !!!</button>
+      <div class="show-number">{{num}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      showModal: false,
+      num: 0
+    };
+  },
+  components: {
+    Modal
+  },
+  methods: {
+    addOne() {
+      this.num++;
+      this.showModal = false;
+    },
+    subOne() {
+      this.num--;
+      this.showModal = false;
+    }
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap");
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: inherit;
+  box-sizing: inherit;
+}
+
+html {
+  font-size: 62.5%;
+}
+body {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+  position: relative;
+  height: 100vh;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 25%;
+  transform: translateY(-25%);
+  text-align: center;
+}
+
+.viewModal {
+  border: none;
+  cursor: pointer;
+  background: none;
+  font-size: 1.6rem;
+  color: rgb(0, 0, 0);
+  padding: 8px 20px;
+  /* margin-right: 20px; */
+  outline: none;
+  border: 1px solid grey;
+  border-radius: 100px;
+}
+
+.show-number {
+  margin-top: 2.5rem;
+  font-size: 4rem;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,123 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <h2>😨 Are you sure you want to do this?</h2>
+          <p>Please note your actions.</p>
+          <div class="modal-buttons">
+            <button @click="addOne">Add One</button>
+            <button @click="subOne">Sub One</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: ["num"],
+  methods: {
+    addOne() {
+      this.$emit("addOne");
+    },
+    subOne() {
+      this.$emit("subOne");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 25%;
+  margin: 0px auto;
+  padding: 30px 30px;
+  background-color: rgb(27, 27, 27);
+  border-radius: 5px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  color: #fff;
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+
+.modal-container h2 {
+  font-size: 2rem;
+  margin-top: 0;
+  color: #ffffff;
+  font-weight: Bold;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.116);
+  padding-bottom: 20px;
+}
+
+.modal-container p {
+  opacity: 0.5;
+  font-size: 1.8rem;
+  margin-top: 20px;
+  color: #ffffff;
+}
+
+.modal-buttons {
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+}
+
+.modal-buttons button {
+  background: none;
+  font-size: 1.6rem;
+  color: #fff;
+  padding: 8px 20px;
+  margin-right: 20px;
+  outline: none;
+  border: 1px solid grey;
+  border-radius: 100px;
+  cursor: pointer;
+}
+
+.modal-buttons button:last-child {
+  background-color: #ff1a55;
+  border: 1px solid #ff1a55;
+}
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+
+@media only screen and (max-width: 1300px) {
+  .modal-container {
+    width: 50%;
+  }
+}
+</style>
